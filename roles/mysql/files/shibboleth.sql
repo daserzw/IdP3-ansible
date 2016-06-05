ba3316e160cea98a3fc72ae17cadf17ac428cb26
Davide Vaghetti <davide.vaghetti@gmail.com>
2016-06-05 08:08:19 +0200

    INSERT RuoliOrganizzativi not in shibboleth.sql

--- roles/mysql/files/shibboleth.sql
+++ roles/mysql/files/shibboleth.sql
@@ -41,8 +41,5 @@ ruolo VARCHAR(255) NOT NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO RuoliOrganizzativi (id, uid,ruolo) VALUES
-       (0, 'mario', 'Tecnico'),(1, 'pino', 'Amministrativo'),
-       (2, 'pina', 'Docente');
 
 
